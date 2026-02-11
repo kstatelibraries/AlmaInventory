ded982cbee32335b90d3c45d5a7a7effb0f4a80c
Thomas Misilo <tmisilo@ksu.edu>
2026-02-11 10:28:40 -0600

    Enable remote ip for Apache

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,4 +1,8 @@
 FROM php:8.1.2-apache
+RUN a2enmod remoteip \
+ && echo "RemoteIPHeader X-Forwarded-For" > /etc/apache2/conf-available/remoteip.conf \
+ && echo "RemoteIPInternalProxy 10.0.0.0/8" >> /etc/apache2/conf-available/remoteip.conf \
+ && a2enconf remoteip
 COPY webapp/* /var/www/html/
 COPY php/* /var/www/html/php/
 COPY php/barcode.init.js .
